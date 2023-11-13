98b16492114a42a22dfbfdcdb0dd9aef1b32ac89
AndersPanders123 <123541105+AndersPanders123@users.noreply.github.com>
2023-11-13 21:13:36 +0100

    g

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ word = ""
 def any_key_pressed(keyboard_event):
     global word  # Use the global keyword to access the global variable
     word += "Get hacc nuub\n"
+    result_label.config(text=word)
 
 # Create the Tkinter window
 window = tk.Tk()
@@ -14,7 +15,7 @@ window.geometry("1280x720")
 window.configure(bg="black")
 
 # Create a label to display the result
-result_label = tk.Label(window, fg="green",bg="black", font=("Arial", 18, "bold"))
+result_label = tk.Label(window, fg="green",bg="black", font=("Arial", 12, "bold"))
 result_label.pack(pady=20, anchor="w")  # Use anchor="w" to align text to the left
 
 # Bind the any_key_pressed function to any key press event
@@ -22,5 +23,3 @@ keyboard.hook(any_key_pressed)
 
 # Start the Tkinter event loop
 window.mainloop()
-
-
