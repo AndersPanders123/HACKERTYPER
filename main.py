b1f16c5e452f407536d7e19bd30d1966d82fd8a6
AndersPanders123 <123541105+AndersPanders123@users.noreply.github.com>
2023-11-16 08:39:08 +0100

    ok

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import tkinter as tk
 import keyboard
-from PIL import Image, ImageTk
 
 word = ""
 Percentage = 0
@@ -14,17 +13,10 @@ def any_key_pressed(keyboard_event):
     Percentage += 0.1
 
     if int(Percentage) >= 101:
-        path = os.getcwd() + "\\003 leker med python\\hackertyper\\HACKERTYPER\\anders18.png"
-        image1 = Image.open(path)  # Correct the image file extension
-        test = ImageTk.PhotoImage(image1)
-        label1 = tk.Label(window, image=test)
-        label1.image = test
-        label1.pack() 
-
         exit(0)
 
 window = tk.Tk()
-window.title("HackerTyper")
+window.title("HaccerTyper")
 window.geometry("1280x720")
 window.configure(bg="black")
 
